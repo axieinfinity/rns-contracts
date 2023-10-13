d378ab1fb4cc49e04403a655e8c24083c3456289
TuDo1403 <tudo.dev@gmail.com>
2023-10-13 14:05:06 +0700

    feat: reuse LibRNSDomain & LibString and minor refactor

--- src/libraries/LibString.sol
+++ src/libraries/LibString.sol
@@ -1,13 +1,43 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-library LibStrAddrConvert {
+library LibString {
   error InvalidStringLength();
   error InvalidCharacter(bytes1 char);
 
   /// @dev Lookup constant for method. See more detail at https://eips.ethereum.org/EIPS/eip-181
-
   bytes32 private constant LOOKUP = 0x3031323334353637383961626364656600000000000000000000000000000000;
+
+  /**
+   * @dev Returns the length of a given string
+   *
+   * @param s The string to measure the length of
+   * @return The length of the input string
+   */
+  function strlen(string memory s) internal pure returns (uint256) {
+    unchecked {
+      uint256 i;
+      uint256 len;
+      uint256 bytelength = bytes(s).length;
+      for (len; i < bytelength; len++) {
+        bytes1 b = bytes(s)[i];
+        if (b < 0x80) {
+          i += 1;
+        } else if (b < 0xE0) {
+          i += 2;
+        } else if (b < 0xF0) {
+          i += 3;
+        } else if (b < 0xF8) {
+          i += 4;
+        } else if (b < 0xFC) {
+          i += 5;
+        } else {
+          i += 6;
+        }
+      }
+      return len;
+    }
+  }
 
   /**
    * @dev Converts an address to string.
@@ -38,8 +68,8 @@ library LibStrAddrConvert {
       uint160 addr;
       for (uint256 i = 0; i < 40; i += 2) {
         addr *= 0x100;
-        addr += uint160(_hexCharToDec(bytes(stringifiedAddr)[i])) * 0x10;
-        addr += _hexCharToDec(bytes(stringifiedAddr)[i + 1]);
+        addr += uint160(hexCharToDec(bytes(stringifiedAddr)[i])) * 0x10;
+        addr += hexCharToDec(bytes(stringifiedAddr)[i + 1]);
       }
       return address(addr);
     }
@@ -49,7 +79,7 @@ library LibStrAddrConvert {
    * @dev Converts a hex char (0-9, a-f, A-F) to decimal number.
    * Reverts if the char is invalid.
    */
-  function _hexCharToDec(bytes1 c) private pure returns (uint8 r) {
+  function hexCharToDec(bytes1 c) private pure returns (uint8 r) {
     unchecked {
       if ((bytes1("a") <= c) && (c <= bytes1("f"))) r = uint8(c) - 87;
       else if ((bytes1("A") <= c) && (c <= bytes1("F"))) r = uint8(c) - 55;
