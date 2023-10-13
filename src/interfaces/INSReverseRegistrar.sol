d3e1e62bcbcaab5c08069e6117f64c6fae4e1b17
TuDo1403 <tudo.dev@gmail.com>
2023-10-13 16:19:57 +0700

    resolve conflicts

--- src/interfaces/INSReverseRegistrar.sol
+++ src/interfaces/INSReverseRegistrar.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import { INameResolver } from "./resolvers/INameResolver.sol";
