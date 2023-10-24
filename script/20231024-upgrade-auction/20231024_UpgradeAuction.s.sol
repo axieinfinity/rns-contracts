d596a17a217e788b231924b2479df802e89387d5
TuDo1403 <tudo.dev@gmail.com>
2023-10-24 11:39:11 +0700

    format: rename script

--- script/20231024-upgrade-auction/20231024_UpgradeAuction.s.sol
+++ script/20231024-upgrade-auction/20231024_UpgradeAuction.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import { ContractKey } from "foundry-deployment-kit/configs/ContractConfig.sol";
 import { RNSDeploy } from "script/RNSDeploy.s.sol";
 
-contract Migration__20231021_UpgradeAuction is RNSDeploy {
+contract Migration__20231024_UpgradeAuction is RNSDeploy {
   function run() public trySetUp {
     _upgradeProxy(ContractKey.RNSAuction, EMPTY_ARGS);
   }
