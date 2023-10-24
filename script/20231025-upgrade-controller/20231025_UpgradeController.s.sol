19ed00e7e095a6e526f80de39686107cdb179590
TuDo1403 <tudo.dev@gmail.com>
2023-10-24 17:40:30 +0700

    chore: rename migration script

--- script/20231025-upgrade-controller/20231025_UpgradeController.s.sol
+++ script/20231025-upgrade-controller/20231025_UpgradeController.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import { ContractKey } from "foundry-deployment-kit/configs/ContractConfig.sol";
 import { RNSDeploy } from "script/RNSDeploy.s.sol";
 
-contract Migration__20231024_UpgradeController is RNSDeploy {
+contract Migration__20231025_UpgradeController is RNSDeploy {
   function run() public trySetUp {
     _upgradeProxy(ContractKey.RONRegistrarController, EMPTY_ARGS);
   }
