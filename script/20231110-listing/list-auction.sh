d266d223727dc5f4487ca349b9a8efd049c0ac6c
TuDo1403 <tudo.dev@gmail.com>
2023-11-13 17:06:42 +0700

    feat: update list-auction script

--- script/20231110-listing/list-auction.sh
+++ script/20231110-listing/list-auction.sh
@@ -1,34 +1,66 @@
-# Define an array of indices [0, 1, 2]
-indices=(0)
+RPC=https://saigon-archive.roninchain.com/rpc
+FROM=0x968D0Cd7343f711216817E617d3f92a23dC91c07
+TARGET=0xb962eddeD164f55D136E491a3022246815e1B5A8
+CURRENT_GAS_PRICE=$(cast gas-price --rpc-url $RPC)
+CURRENT_NONCE=$(cast nonce --rpc-url $RPC $FROM)
+PK=$(op read "op://SC Vault/Testnet Admin/private key")
 
+gasPrice=$((CURRENT_GAS_PRICE + 1000000000))
+# Define an array of indices [0, 1, 2]
+indices=(1 2)
 # Loop through each index
 for index in "${indices[@]}"; do
-    # Read the JSON file
-    jsonData=$(cat "script/20231110-listing/data/AuctionNames${index}.json")
+    (
+        namehashResults=()
+        # Read the JSON file
+        jsonData=$(cat "script/20231110-listing/data/AuctionNames${index}.json")
 
-    # Parse JSON data
-    labelsString=$(
-        node -e '
-            const labels = JSON.parse(process.argv[1]).auctionNames.map(auction => auction.label);
-            console.log(labels.join(","));
+        # Parse JSON data
+        labels=$(
+            node -e '
+            const labels = JSON.parse(process.argv[1]).auctionNames.map(auction => auction.label + ".ron");
+            labels.forEach(v => console.log(v))
         ' "$jsonData"
-    )
-    startingPrices=$(
-        node -e '
+        )
+        startingPrices=$(
+            node -e '
             const startingPrices = JSON.parse(process.argv[1]).auctionNames.map(auction => auction.startingPrice);
             console.log(startingPrices.join(","));
         ' "$jsonData"
-    )
+        )
 
-    startedAt=$(echo "$jsonData" | jq -r '.auctionEvent.startedAt')
-    endedAt=$(echo "$jsonData" | jq -r '.auctionEvent.endedAt')
+        # Loop through each label and call cast namehash
+        for label in ${labels}; do
+            result=$(cast namehash "$label")
+            echo "Label: $label, Namehash: $result"
+            namehashResults+=($result)
+        done
 
-    # Print the extracted values
-    echo event started at: $startedAt
-    echo event ended at: $endedAt
+        # Join array elements with ","
+        joinedString=$(
+            IFS=,
+            echo "${namehashResults[*]}"
+        )
 
-    auctionId=$(cast call --from 0x0f68edbe14c8f68481771016d7e2871d6a35de11 --rpc-url https://api-partner.roninchain.com/rpc 0xD55e6d80aeA1FF4650BC952C1653ab3CF1b940A9 "createAuctionEvent((uint256,uint256))" "($startedAt, $endedAt)")
-    echo auctionId $auctionId
-    listNamesForAuctionGas=$(cast e --from 0x0f68edbe14c8f68481771016d7e2871d6a35de11 --rpc-url https://api-partner.roninchain.com/rpc 0xD55e6d80aeA1FF4650BC952C1653ab3CF1b940A9 "listNamesForAuction(bytes32,uint256[],uint256[])" $auctionId "[$labelsString]" "[$startingPrices]")
-    echo "listNamesForAuctionGas" $listNamesForAuctionGas
+        auctionId=$(echo "$jsonData" | jq -r '.auctionId')
+
+        nextNonce=$((CURRENT_NONCE + index - 1))
+        echo Nonce: $nextNonce
+
+        echo auctionId $auctionId
+        echo startingPrices $startingPrices
+
+        # fee=$(cast e --from $FROM --rpc-url $RPC $TARGET "listNamesForAuction(bytes32,uint256[],uint256[])" "$auctionId" "[$joinedString]" "[$startingPrices]")
+        # echo fee: $fee
+
+        txHash=$(cast s --gas-price $gasPrice --async --confirmations 0 --nonce $nextNonce --legacy --from $FROM --private-key $PK --rpc-url $RPC $TARGET "listNamesForAuction(bytes32,uint256[],uint256[])" "$auctionId" "[$joinedString]" "[$startingPrices]")
+        echo https://saigon-app.roninchain.com/tx/$txHash
+    ) &
+
+    # Check if index is a multiple of 100, then wait
+    if [ $((index % 50)) -eq 0 ]; then
+        wait
+    fi
 done
+
+wait
