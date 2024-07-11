0d1fee90606e22d2d590221d4a5388ca491b6691
tringuyenskymavis <tri.minh.nguyen@skymavis.com>
2024-07-11 11:47:55 +0700

    chore: update script

--- script/20240705-migrate-aution-and-controller-treasury/20240705_MigrateAuctionAndControllerTreasury.s.sol
+++ script/20240705-migrate-aution-and-controller-treasury/20240705_MigrateAuctionAndControllerTreasury.s.sol
@@ -9,12 +9,11 @@ import { Contract } from "script/utils/Contract.sol";
 import { Migration } from "script/Migration.s.sol";
 import { RNSCommission, RNSCommissionDeploy } from "script/contracts/RNSCommissionDeploy.s.sol";
 
-contract Migration__20240507_MigrateAuctionAndControllerAdminAndTreasuryMainnet is Migration {
+contract Migration__20240507_MigrateAuctionAndControllerTreasuryMainnet is Migration {
   RONRegistrarController private _controller;
   RNSAuction private _auction;
   RNSCommission private _commission;
   address private _currentAdmin;
-  address private _nextAdmin;
   bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
 
   function run() public {
@@ -22,25 +21,17 @@ contract Migration__20240507_MigrateAuctionAndControllerAdminAndTreasuryMainnet 
     _controller = RONRegistrarController(loadContract(Contract.RONRegistrarController.key()));
     _commission = RNSCommission(loadContract(Contract.RNSCommission.key()));
     _currentAdmin = 0x0F68eDBE14C8f68481771016d7E2871d6a35DE11;
-    _nextAdmin = 0x9D05D1F5b0424F8fDE534BC196FFB6Dd211D902a;
 
     vm.startBroadcast(_currentAdmin);
     _auction.setTreasury(payable(address(_commission)));
     _controller.setTreasury(payable(address(_commission)));
-
-    _auction.grantRole(DEFAULT_ADMIN_ROLE, _nextAdmin);
-    _controller.grantRole(DEFAULT_ADMIN_ROLE, _nextAdmin);
-
-    _auction.revokeRole(DEFAULT_ADMIN_ROLE, _currentAdmin);
-    _controller.revokeRole(DEFAULT_ADMIN_ROLE, _currentAdmin);
     vm.stopBroadcast();
   }
 
   function _postCheck() internal override {
     _validateTreasuryAddress();
-    _validateNewAdmin();
-    _validatePrevAdminIsRevoked();
-    _validateAdminCount();
+    _validateSendersAddress();
+    _validateSendMoneyToCommission();
   }
 
   function _validateTreasuryAddress() internal logFn("_validateTreasuryAddress") {
@@ -48,20 +39,29 @@ contract Migration__20240507_MigrateAuctionAndControllerAdminAndTreasuryMainnet 
     assertEq(_controller.getTreasury(), address(_commission));
   }
 
-  function _validateNewAdmin() internal logFn("_validateNewAdmin") {
-    address newAdmin = _nextAdmin;
-    require(_auction.hasRole(DEFAULT_ADMIN_ROLE, newAdmin));
-    require(_controller.hasRole(DEFAULT_ADMIN_ROLE, newAdmin));
+  function _validateSendersAddress() internal logFn("_validateSendersAddress") {
+    bytes32 SENDER_ROLE = keccak256("SENDER_ROLE");
+
+    require(_commission.hasRole(SENDER_ROLE, address(_auction)));
+    require(_commission.hasRole(SENDER_ROLE, address(_controller)));
   }
 
-  function _validatePrevAdminIsRevoked() internal logFn("_validatePrevAdminIsRevoked") {
-    address prevAdmin = _currentAdmin;
-    require(!_auction.hasRole(DEFAULT_ADMIN_ROLE, prevAdmin));
-    require(!_controller.hasRole(DEFAULT_ADMIN_ROLE, prevAdmin));
-  }
+  function _validateSendMoneyToCommission() internal logFn("_validateSendMoneyToCommission") {
+    vm.deal(address(_auction), 100 ether);
+    vm.prank(address(_auction));
+    address(_commission).call{ value: 100 ether }("");
 
-  function _validateAdminCount() internal logFn("_validateAdminCount") {
-    assertEq(_auction.getRoleMemberCount(DEFAULT_ADMIN_ROLE), 1);
-    assertEq(_controller.getRoleMemberCount(DEFAULT_ADMIN_ROLE), 1);
+    vm.deal(address(_controller), 100 ether);
+    vm.prank(address(_controller));
+    address(_commission).call{ value: 100 ether }("");
+
+    assertEq(address(_commission).balance, 0 ether);
+
+    address randomAddr = makeAddr("random address");
+    vm.deal(address(randomAddr), 100 ether);
+    vm.prank(randomAddr);
+    address(_commission).call{ value: 100 ether }("");
+
+    assertEq(address(_commission).balance, 100 ether);
   }
 }
