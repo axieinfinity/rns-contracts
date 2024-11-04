94103fc63953384a25ed9576701f812be6424a75
TuDo1403 <tudo.dev@gmail.com>
2024-11-04 17:51:34 +0700

    script: update rpc

--- script/operations/bulk-renew.mjs
+++ script/operations/bulk-renew.mjs
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const provider = new ethers.JsonRpcProvider("https://api-archived.roninchain.com/rpc");
+const provider = new ethers.JsonRpcProvider(process.env.RONIN_MAINNET_RPC);
 const wallet = new ethers.Wallet(process.env.RONIN_MAINNET_PK, provider);
 const renewList = JSON.parse(fs.readFileSync("renew-list.json", "utf8"));
 // Verify: https://app.roninchain.com/address/0x662852853614cbbb5d04bf2e29955b97e3c50b69
@@ -20,10 +20,10 @@ console.log(`Current nonce: ${nonce}`);
 const account = wallet.address;
 const shouldSimulate = true;
 // Assert the list is unique
-// if (new Set(renewList).size !== renewList.length) {
-// 	console.error("List is not unique");
-// 	process.exit(1);
-// }
+if (new Set(renewList).size !== renewList.length) {
+	console.error("List is not unique");
+	process.exit(1);
+}
 const contract = new ethers.Contract(ronRegistrarControllerAddr, abi, provider);
 const chunkSize = 20;
 
