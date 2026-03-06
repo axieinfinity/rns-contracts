c92cbd63c72387c885a3cd51ba1d2af7d64f37d7
huyhuynh3103 <huy.huynh@skymavis.com>
2026-03-06 16:30:52 +0700

    chore: upgrade testnet

--- script/20260306-rns-upgrade/20260306_RNSUpgrade.s.sol
+++ script/20260306-rns-upgrade/20260306_RNSUpgrade.s.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.19;
 import { Contract } from "script/utils/Contract.sol";
 import { Migration } from "script/Migration.s.sol";
 
-contract Migration__20231020_RNSUpgrade is Migration {
+contract Migration__20260306_RNSUpgrade is Migration {
   function run() public {
-    _upgradeProxy(Contract.RNSUnified.key());
+    _upgradeProxy(Contract.RNSAuction.key());
+    _upgradeProxy(Contract.RNSDomainPrice.key());
   }
 }
